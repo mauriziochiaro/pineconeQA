d0a3dbdc48417ef99f1696968432ff3d1a3d69db
mauriziochiaro <116275001+mauriziochiaro@users.noreply.github.com>
2023-03-20 18:12:10 +0100

    Add files via upload

---
+++ autorizzazione.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from datetime import date
+import http.client
+
+MAX_EXECUTIONS_PER_CLIENT = 10
+
+@st.cache_resource
+def get_executions():
+    return {}
+
+def autorizzazione_utente():
+    # Function to get the client's IP address
+    def get_client_ip():
+        conn = http.client.HTTPConnection("ifconfig.me")
+        conn.request("GET", "/ip")
+        response = conn.getresponse()
+        return response.read().decode("utf-8")
+
+    executions = get_executions()
+
+    # Get the current date
+    current_date = date.today()
+
+    # Get the client's IP address
+    client_ip = get_client_ip()
+
+    if client_ip not in executions:
+        executions[client_ip] = {"executions": 0, "last_execution_date": current_date}
+
+    # Check if the current date is different from the last execution date and reset the counter if needed
+    if executions[client_ip]["last_execution_date"] != current_date:
+        executions[client_ip]["executions"] = 0
+        executions[client_ip]["last_execution_date"] = current_date
+
+    print(executions[client_ip])
+    # Check if the client has reached the maximum number of script executions
+    if executions[client_ip]["executions"] <= MAX_EXECUTIONS_PER_CLIENT:
+        executions[client_ip]["executions"] += 1
+
+        st.success("Buongiorno! Sono LexE, l'assistente AI per il software Progetto INTEGRA. Sono istruita per rispondere a domande tecniche sull'utilizzo del software. Come posso aiutarla?")  
+
+        st.info("Numero di richieste giornaliere effettuate: {} su {}".format(executions[client_ip]["executions"]-1, MAX_EXECUTIONS_PER_CLIENT))
+
+        return True
+
+    else:
+        st.warning("Raggiunto numero massimo di richieste giornaliere disponibili: ({}).".format(MAX_EXECUTIONS_PER_CLIENT))
+
+        return False
---
+++ vectorize_pdf_to_Pinecone.py
@@ -0,0 +1,37 @@
+import os
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+loader = UnstructuredPDFLoader("D:\\path\\to\\help.pdf")
+data = loader.load()
+print (f'You have {len(data)} document(s) in your data')
+print (f'There are {len(data[0].page_content)} characters in your document')
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(data)
+print (f'Now you have {len(texts)} documents')
+
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
+PINECONE_API_ENV = os.environ["PINECONE_API_ENV"]
+
+# OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+# PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+# PINECONE_API_ENV = "us-east-1-aws"
+
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+# initialize pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_API_ENV  # next to api key in console
+)
+# create new index app.pinecone.io
+index_name = "indexname"
+
+# populate vectore db 
+docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
