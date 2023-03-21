f8f1d903436875ae2f4e764266f43cd1aba7fcde
mauriziochiaro <116275001+mauriziochiaro@users.noreply.github.com>
2023-03-21 16:09:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ st.set_page_config(page_title="LexE", page_icon=":robot:")
 #             </style>
 #             """
 
-# source: https://discuss.streamlit.io/t/remove-made-with-streamlit-from-bottom-of-app/1370/2
-hide_streamlit_style = """
-            <style>
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.header("Chat Assistenza Progetto INTEGRA")
+# # source: https://discuss.streamlit.io/t/remove-made-with-streamlit-from-bottom-of-app/1370/2
+# hide_streamlit_style = """
+#             <style>
+#             footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.header("Assistenza Progetto INTEGRA")
 
 # OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 # PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
@@ -60,13 +61,70 @@ if "questions" not in st.session_state:
     st.session_state.questions = []
 # if "chat_cleared" not in st.session_state:
 #     st.session_state.chat_cleared = False    
-        
+
+# DEMO Q&A:
+demo_inst_q="Come si installa Progetto INTEGRA?"
+demo_inst_a='''Per installare Progetto INTEGRA, è necessario seguire i seguenti passaggi:
+1. Se si desidera installare il programma su un singolo PC, selezionare un percorso locale (ad esempio C:\Integra511) durante la selezione della cartella di installazione. Se si desidera installare il programma su più PC condividendo gli archivi su un server, selezionare uno spazio condiviso come lettera di unità visualizzato univocamente da tutti gli utenti (ad esempio "M:\"), specificando il percorso del server durante la selezione della cartella di installazione.
+
+2. Inserire il proprio numero di licenza (n. 5 caratteri numerici) ed i propri dati identificativi.
+
+3. Indicare il percorso librerie (cartella "\lib" contenente tutti gli archivi grafici e i files dwg/xls) e il percorso Apparecchiature e prezzari pubblici (cartella "\libbd" contenente tutte le basi dati ed i prezzari). È consigliabile inserire entrambe le cartelle sul server all'interno di una directory che identifica la versione attuale (ad esempio M:\Integra\lib e M:\Integra\libbd).
+
+4. Premere Avanti per proseguire fino a terminare l'installazione.
+
+5. Dopo l'installazione, è possibile eseguire il programma dal PC pilota. Per eseguire il programma da altre postazioni, è necessario creare un Operatore per ogni utilizzatore tramite la procedura che permette di gestire gli Operatori e realizzare il collegamento a Progetto Integra (collegamento al file integra5_64.exe presente sul Server) su ogni altra postazione.
+'''
+
+demo_bd_q="Domanda: Come creo una BD personalizzata?"
+demo_bd_a='''Per creare una base dati personalizzata in Progetto INTEGRA, segui i seguenti passaggi:
+
+1. Apri la base dati CaviCustom.bd tramite il menu File Apri Base dati CaviCustom.bd (dal percorso di installazione che, di default, è il seguente: C:\Integra\libbd);
+2. Salvala utilizzando il comando Salva come con un altro nome (ad esempio “CaviCustom-il proprio numero di licenza” che è univoco);
+3. Quindi, dal menu Modifica Impostazioni di progetto, seleziona il disco di backup di tutti i dati personalizzati che non possono essere disponibili facendo semplicemente una nuova installazione del programma.
+
+In questo modo avrai creato una nuova base dati personalizzata.
+'''
+
+demo_ge_q="Domanda: Come inserisco un Gruppo Elettrogeno?"
+demo_ge_a='''Per inserire un Gruppo Elettrogeno in un impianto già esistente, è possibile utilizzare la funzione "Collega a nuovo GE" presente nel pulsante Elaborazioni (Ingranaggio) oppure seguire la procedura di seguito riportata:
+
+1. Aggiungere una nuova Fornitura tramite il pulsante "Aggiungi" a destra.
+2. Selezionare "Gruppo Elettrogeno" dall'apposito sottomenu di selezione.
+3. Inserire i dati descrittivi di identificazione e i dati tecnici determinati dall'impianto a monte del quadro principale, in particolare la corrente di cortocircuito.
+4. Confermare i dati inseriti.
+5. Il programma realizzerà automaticamente l'impianto impostando la commutazione con le due forniture.
+
+In alternativa, se il progetto è già stato realizzato, è possibile aggiungere una commutazione Rete-Gruppo tramite la procedura sopra descritta, ma occorre prima aggiungere una Fornitura di tipo Gruppo Elettrogeno con al di sotto una quadro generale con una partenza.
+'''
+
+# you can create columns to better manage the flow of your page
+# this command makes 3 columns of equal width
+col1, col2, col3 = st.columns(3)
+with col1:
+# Add an example button 
+    btn_demo1=st.button(label="Come si installa Progetto INTEGRA?",key="demo_inst")
+if btn_demo1:
+    RELOAD=True
+    st.markdown(f'<p style="font-weight: bold; display: inline;"><em>Domanda: {demo_inst_q}</em><br /><br /></p> {demo_inst_a}', unsafe_allow_html=True)
+# this will put a button in the middle column
+with col2:
+    btn_demo2=st.button(label="Come creo una BD personalizzata?",key="demo_bd")
+if btn_demo2:
+    RELOAD=True
+    st.markdown(f'<p style="font-weight: bold; display: inline;"><em>Domanda: {demo_bd_q}</em><br /><br /></p> {demo_bd_a}', unsafe_allow_html=True)   
+with col3:
+    btn_demo3=st.button(label="Come inserisco un Gruppo Elettrogeno?",key="demo_ge")
+if btn_demo3:
+    RELOAD=True
+    st.markdown(f'<p style="font-weight: bold; display: inline;"><em>Domanda: {demo_ge_q}</em><br /><br /></p> {demo_ge_a}', unsafe_allow_html=True)    
+
 AUTORIZZATO = auth.autorizzazione_utente(RELOAD)
 
 if AUTORIZZATO:         
     placeholder = st.empty()
     def get_text():
-        input_text = placeholder.text_input("You: ", value="", key="input")
+        input_text = placeholder.text_input("Domanda: ", value="", key="input")
         return input_text
 
     user_input = get_text()
@@ -81,13 +139,16 @@ if AUTORIZZATO:
     # if st.button("Nuova chat"):
     #     clear_conversation()
 
-    if user_input != "" and not st.session_state.chat_cleared:
+    # if user_input != "" and not st.session_state.chat_cleared:
+    if user_input != "" :
         
         # REFERENCE SOURCE:
         # OpenAI's New GPT 3.5 Embedding Model for Semantic Search => https://youtu.be/ocxq84ocYi0
         # OpenAI GPT 3.5 AI assistant with Langchain + Pinecone #1 => https://youtu.be/15TDwVSpwKc
         index = pinecone.Index(index_name=index_name)
-        def integra_bot(query: str): 
+        
+        def integra_bot(query: str):
+            
             try:       
                 model='text-embedding-ada-002'
                 res=openai.Embedding.create(
@@ -127,8 +188,5 @@ if AUTORIZZATO:
                 st.markdown(f'<p style="font-weight: bold; display: inline;"><em>Domanda: {st.session_state.questions[i]}</em><br /><br /></p> {st.session_state.answers[i]}', unsafe_allow_html=True)
                 if i != 0:
                     st.markdown("---")
-
-# Reset the chat_cleared variable after displaying the answers
-st.session_state.chat_cleared = False            
-
-  
+# # Reset the chat_cleared variable after displaying the answers
+# st.session_state.chat_cleared = False  
